4a5d2abf8834b7b7c0e3ff19fecbe5b09d0f781a
ImLauraGS <lgilsolano@gmail.com>
2024-07-08 12:14:43 +0200

    login and logout done

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -45,4 +45,51 @@ class UserController extends Controller
         }
     }
 
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'validation_errors' => $validator->messages(),
+            ], 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'msg' => 'Usuario o contraseña incorrectos'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $cookie = cookie('token', $token, 60 * 24);
+
+        return response()->json([
+            'msg' => 'Usuario conectado exitosamente',
+            'user' => $user,
+            'token' => $token
+        ], 200)->withCookie($cookie);
+    }
+    public function logout(Request $request)
+{
+    try {
+        $user = $request->user();
+
+        if (!$user) {
+            return response()->json(['status' => 0, 'msg' => 'No user authenticated'], 401);
+        }
+
+        $user->tokens()->delete();
+        return response()->json(['status' => 1, 'msg' => 'Sesión cerrada correctamente'], 200);
+    } catch (\Exception $e) {
+        return response()->json(['status' => 0, 'msg' => 'Error al cerrar sesión'], 500);
+    }
+}
+
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [UserController::class, 'logout']);
+});
